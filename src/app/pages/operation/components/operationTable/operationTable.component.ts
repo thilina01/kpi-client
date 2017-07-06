1e2a415a9065f1764f2467cdb7232a3e752b9e33
Thilina Ranathunga <r.thilina@gmail.com>
2017-07-06 14:28:48 +0530

    Paginator problem fixed

--- src/app/pages/operation/components/operationTable/operationTable.component.ts
+++ src/app/pages/operation/components/operationTable/operationTable.component.ts
@@ -35,18 +35,21 @@ export class OperationTable {
     this.getSections();
     this.getShifts();
   }
+  
   getSections(): void {
     this.sectionService.getCombo().then(sections => {
       this.sections = sections;
       this.sections.unshift({ id: 0, "code": "ALL", "name": "All Sections" });
     });
   }
+
   getShifts(): void {
     this.shiftService.getCombo().then(shifts => {
       this.shifts = shifts;
       this.shifts.unshift({ id: 0, "code": "ALL", "name": "All Shifts" });
     });
   }
+
   loadData() {
     this.service.getPage(0, 20).then((data: any) => {
       this.rows = data.content;
@@ -56,33 +59,13 @@ export class OperationTable {
 
   lazy(event: any, table: any) {
     console.log(event);
-    //const search = table.globalFilter ? table.globalFilter.value : null;
     this.search((event.first / event.rows), event.rows);
-    // if (this.startDate != undefined && this.endDate != undefined && this.section != undefined && this.section.id != undefined) {
-    //   if (this.section.id == 0) {
-    //     this.service.getByProductionDurationAndShiftPage(this.sharedService.YYYYMMDD(this.startDate), this.sharedService.YYYYMMDD(this.endDate), this.shift.id, (event.first / event.rows), event.rows).then((data: any) => {
-    //       this.rows = data.content;
-    //       this.totalRecords = data.totalElements;
-    //     });
-    //   } else {
-    //     this.service.getBySectionAndProductionDurationAndShiftPage(this.section.id, this.sharedService.YYYYMMDD(this.startDate), this.sharedService.YYYYMMDD(this.endDate), this.shift.id, (event.first / event.rows), event.rows).then((data: any) => {
-    //       this.rows = data.content;
-    //       this.totalRecords = data.totalElements;
-    //     });
-    //   }
-    // } else {
-    //   this.service.getPage((event.first / event.rows), event.rows).then((data: any) => {
-    //     //this.service.getBySectionAndProductionDateAndShiftPage(this.section.id, this.sharedService.YYYYMMDD(this.productionDate), this.shift.id, (event.first / event.rows), event.rows).then((data: any) => {
-    //     this.rows = data.content;
-    //     this.totalRecords = data.totalElements;
-    //   });
-    // }
   }
 
   onPage(event) {
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
-      console.log('paged!', event);
+      this.search(event.first, event.rows);
     }, 100);
   }
 
@@ -93,7 +76,6 @@ export class OperationTable {
       this.section.id != undefined &&
       this.shift != undefined &&
       this.shift.id != undefined) {
-      //alert(this.section.id + " ~ " + this.shift.id);
       if (this.section.id == 0 && this.shift.id == 0) {
         this.service.getByProductionDurationPage(this.sharedService.YYYYMMDD(this.startDate), this.sharedService.YYYYMMDD(this.endDate), first, pageSize).then((data: any) => {
           this.fillTable(data);
@@ -112,41 +94,15 @@ export class OperationTable {
           this.fillTable(data);
         });
       }
+    } else {
+      this.service.getPage(first, pageSize).then((data: any) => {
+        this.fillTable(data);
+      });
     }
   }
 
   fillTable(data: any) {
     this.rows = data.content;
     this.totalRecords = data.totalElements;
-    // let paging = {
-    //   first: (0),
-    //   rows: this.dataTable.rows
-    // };
-    // let timer = Observable.timer(100);
-    // timer.subscribe(t => {
-    //   this.dataTable.paginate(paging);
-    // });
-  }
-
-  onRowDblclick(data: any): void {
-    this.router.navigate(['/pages/operation/form/' + data.id]);
-  }
-
-  navigateToForm(id: any): void {
-    this.router.navigate(['/pages/operation/form/' + id]);
-  }
-
-  delete(id: number) {
-    this.confirmationService.confirm({
-      message: 'Are you sure that you want to Delete?',
-      accept: () => {
-        this.service.delete(id).then(response => {
-          this.sharedService.addMessage({ severity: 'info', summary: 'Deleted', detail: 'Delete success' });
-          //this.msgs.push();
-          this.loadData()
-        }
-        );
-      }
-    });
   }
 }
