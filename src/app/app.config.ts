12415f1648a237dbc67e8c214a60d9861b42ba42
Thilina Ranathunga <r.thilina@gmail.com>
2017-07-13 14:29:30 +0530

    json header centralization removed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { OpaqueToken } from '@angular/core';
 import { Cookie } from 'ng2-cookies/ng2-cookies';
 import { Headers } from '@angular/http';
+import { AuthService } from "./services/auth.service";
 
 export let APP_CONFIG = new OpaqueToken('app.config');
 
 export interface IAppConfig {
   apiEndpoint: string;
-  jsonHeaders: Object;
-  setJsonHeaders(): void;
-  getJsonHeaders(): Headers;
+  //jsonHeaders: Object;
+  //setJsonHeaders(): void;
+  //getJsonHeaders(): Headers;
 }
 
 let hostname = location.hostname;
@@ -17,20 +18,20 @@ let apiEndpoint = hostname === 'kpi.trwlanka.com' ? 'http://tmsapi.trwlanka.com/
 
 
 export const AppConfig: IAppConfig = {
-  //apiEndpoint: 'http://tmsapi.trwlanka.com/'
+  //apiEndpoint: 'http://tmsapi.trwlanka.com/',
   //apiEndpoint: 'http://localhost:8080/file-manager-api/'
   apiEndpoint: apiEndpoint,
-  jsonHeaders: {
-    'Content-Type': 'application/json'
-  },
-  setJsonHeaders(): void {
-    this.email = Cookie.get('email') !== undefined ? Cookie.get('email') : 'admin@trwlanka.com';
-    this.jsonHeaders = {
-      'Content-Type': 'application/json',
-      'email': this.email
-    }
-  },
-  getJsonHeaders(): Headers {
-    return new Headers(this.jsonHeaders);
-  }
+  // jsonHeaders: {
+  //   'Content-Type': 'application/json'
+  // },
+  // setJsonHeaders(): void {
+  //   //this.email = Cookie.get('email') !== undefined ? Cookie.get('email') : 'admin@trwlanka.com';
+  //     this.jsonHeaders = {
+  //       'Content-Type': 'application/json'
+  //       //'email': this.email
+  //     }
+  // },
+  // getJsonHeaders(): Headers {
+  //   return new Headers(this.jsonHeaders);
+  // }
 };
