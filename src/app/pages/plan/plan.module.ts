9ee9e2ecfcd4358c446e804ecd9db05db269e0ae
Thilina Ranathunga <r.thilina@gmail.com>
2017-07-24 17:34:01 +0530

    UI Style changes

--- src/app/pages/plan/plan.module.ts
+++ src/app/pages/plan/plan.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgaModule } from '../../theme/nga.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CalendarModule,AutoCompleteModule } from 'primeng/primeng';
+import { CalendarModule,AutoCompleteModule,PanelModule,ButtonModule } from 'primeng/primeng';
 
 import { MaterialModule } from '@angular/material';
 
@@ -22,6 +22,7 @@ import { PlanFormHr } from './components/planFormHr/planFormHr.component';
     MaterialModule,
     routing,
     FormsModule,
+    PanelModule,
     ReactiveFormsModule
   ],
   declarations: [
