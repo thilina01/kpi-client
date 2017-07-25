f159d2db409865101f82037e4fcc40ecbd753c87
Thilina Ranathunga <r.thilina@gmail.com>
2017-07-25 09:47:34 +0530

    Test moduke added for testing purpose

---
+++ src/app/pages/test/test.routing.ts
@@ -0,0 +1,18 @@
+import { Routes, RouterModule }  from '@angular/router';
+
+import { Test } from './test.component';
+import { TestForm } from './components/testForm/testForm.component';
+import { TestTable } from './components/testTable/testTable.component';
+import { ModuleWithProviders } from '@angular/core';
+
+// noinspection TypeScriptValidateTypes
+export const routes: Routes = [
+  {
+    path: '',
+    component: Test,
+     children: [
+    ]
+  }
+];
+
+export const routing = RouterModule.forChild(routes);
