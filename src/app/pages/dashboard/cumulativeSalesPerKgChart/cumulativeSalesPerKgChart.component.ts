06c273e74457fb3d73d4d9e72c5c24322ead23db
Thilina Ranathunga <r.thilina@gmail.com>
2017-07-27 15:22:29 +0530

    CumulativeSalesPerKg API client and Chart

---
+++ src/app/pages/dashboard/cumulativeSalesPerKgChart/cumulativeSalesPerKgChart.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+import { CumulativeSalesPerKgChartService } from './cumulativeSalesPerKgChart.service';
+import { ChartService } from '../../../services/chart.service';
+
+import 'style-loader!./cumulativeSalesPerKgChart.scss';
+
+@Component({
+  selector: 'cumulative-sales-per-kg-chart',
+  templateUrl: './cumulativeSalesPerKgChart.html'
+})
+export class CumulativeSalesPerKgChart {
+
+  amChart: any;
+
+  constructor(private _cumulativeSalesPerKgChartService: CumulativeSalesPerKgChartService, private chartService: ChartService) {
+    
+    var startDate = new Date();
+    startDate.setMonth(startDate.getMonth() - 6);
+    var monthText :string; 
+    monthText = ((startDate.getMonth()+1)<10?"0"+startDate.getMonth()+1:startDate.getMonth()+1)+"";
+    var startDateText = startDate.getFullYear()+"-"+monthText.slice(-2)+"-01";
+    
+    var endDate = new Date();
+    var endDateText = endDate.getFullYear()+"-"+(endDate.getMonth()<10?"0"+endDate.getMonth():endDate.getMonth())+"-"+(new Date(endDate.getFullYear(), endDate.getMonth(), 0).getDate());
+
+    this.chartService.getMonthlyCumulativeSalesPerKg(startDateText, endDateText).then((data) => {
+      var chartData = this._cumulativeSalesPerKgChartService.getChartData(data);
+      this.amChart = AmCharts.makeChart("cumulativeSalesPerKgchartdiv",chartData);
+    });
+  }
+
+  initChart(chart: any) {
+    let zoomChart = () => {
+      //chart.zoomToDates(new Date(2013, 3), new Date(2014, 0));
+    };
+
+    chart.addListener('rendered', zoomChart);
+    zoomChart();
+
+    if (chart.zoomChart) {
+      chart.zoomChart();
+    }
+  }
+}
