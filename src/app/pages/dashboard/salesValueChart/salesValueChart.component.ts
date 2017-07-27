51f0fc9d9a69ab380c9302a29533c72b88457c1d
Thilina Ranathunga <r.thilina@gmail.com>
2017-07-27 15:27:12 +0530

    SalesValue Chart

---
+++ src/app/pages/dashboard/salesValueChart/salesValueChart.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+import { SalesValueChartService } from './salesValueChart.service';
+import { ChartService } from '../../../services/chart.service';
+
+import 'style-loader!./salesValueChart.scss';
+
+@Component({
+  selector: 'sales-value-chart',
+  templateUrl: './salesValueChart.html'
+})
+export class SalesValueChart {
+
+  amChart: any;
+
+  constructor(private _salesValueChartService: SalesValueChartService, private chartService: ChartService) {
+    
+    var startDate = new Date();
+    startDate.setMonth(startDate.getMonth() - 6);
+    var monthText :string; 
+    monthText = ((startDate.getMonth()+1)<10?"0"+startDate.getMonth()+1:startDate.getMonth()+1)+"";
+    var startDateText = startDate.getFullYear()+"-"+monthText.slice(-2)+"-01";
+    
+    var endDate = new Date();
+    var endDateText = endDate.getFullYear()+"-"+(endDate.getMonth()<10?"0"+endDate.getMonth():endDate.getMonth())+"-"+(new Date(endDate.getFullYear(), endDate.getMonth(), 0).getDate());
+
+    this.chartService.getMonthlySalesValue(startDateText, endDateText).then((data) => {
+      var chartData = this._salesValueChartService.getChartData(data);
+      this.amChart = AmCharts.makeChart("salesValuechartdiv",chartData);
+    });
+  }
+
+  initChart(chart: any) {
+    let zoomChart = () => {
+      //chart.zoomToDates(new Date(2013, 3), new Date(2014, 0));
+    };
+
+    chart.addListener('rendered', zoomChart);
+    zoomChart();
+
+    if (chart.zoomChart) {
+      chart.zoomChart();
+    }
+  }
+}
---
+++ src/app/pages/dashboard/salesValueChart/salesValueChart.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { BaThemeConfigProvider, colorHelper, layoutPaths } from '../../../theme';
+
+@Injectable()
+export class SalesValueChartService {
+
+  constructor(private _baConfig: BaThemeConfigProvider) {
+  }
+
+ getChartData(data) {
+
+    let layoutColors = this._baConfig.get().colors;
+    let graphColor = this._baConfig.get().colors.custom.dashboardSalesValueChart;
+
+    return {
+      "type": "serial",
+      "theme": "blur",
+      "depth3D": 20,
+      "angle": 30,
+      "dataProvider": data/*[
+        { "month": "Nov-16", "value": 90 },
+        { "month": "Dec-16", "value": 95 },
+        { "month": "Jan-17", "value": 94 },
+        { "month": "Feb-17", "value": 88 },
+        { "month": "Mar-17", "value": 93 },
+        { "month": "Apr-17", "value": 88 }
+      ]*/,
+      creditsPosition: 'top-right',
+      "valueAxes": [{
+        minVerticalGap: 50,
+        gridAlpha: 0,
+        color: layoutColors.defaultText,
+        axisColor: layoutColors.defaultText
+      }],
+      "gridAboveGraphs": true,
+      "startDuration": 1,
+      "graphs": [{
+        "balloonText": "Budget: <b>[[budget]]</b>",
+        "fillAlphas": 0.8,
+        "lineAlpha": 0.2,
+        "type": "column",
+        "valueField": "budget",
+        "labelText": "[[budget]]"
+      },{
+        "balloonText": "Actual: <b>[[actual]]</b>",
+        "fillAlphas": 0.8,
+        "lineAlpha": 0.2,
+        "type": "column",
+        "valueField": "actual",
+        "labelText": "[[actual]]"
+      }],
+      "chartCursor": {
+        "categoryBalloonEnabled": false,
+        "cursorAlpha": 0,
+        "zoomable": false
+      },
+      "categoryField": "month",
+      "categoryAxis": {
+        "gridPosition": "start",
+        "gridAlpha": 0,
+        "tickPosition": "start",
+        "tickLength": 20,
+        color: layoutColors.defaultText,
+        axisColor: layoutColors.defaultText
+      }
+    };
+  };
+}
