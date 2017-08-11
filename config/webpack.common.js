86f91386b531049030992de3cf00f4ff94f753fe
Thilina Ranathunga <r.thilina@gmail.com>
2017-08-11 11:31:37 +0530

    Branding

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -25,8 +25,8 @@ const ngcWebpack = require('ngc-webpack');
 const HMR = helpers.hasProcessFlag('hot');
 const AOT = helpers.hasNpmFlag('aot');
 const METADATA = {
-  title: 'ng2-admin - Angular 2 Admin Template',
-  description: 'Free Angular 2 and Bootstrap 4 Admin Template',
+  title: 'OTR-KPI',
+  description: 'Internal operations and performance management system',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
