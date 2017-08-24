9ae3b8cca9d01b8413000b934aae95310157f14b
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-08-24 07:01:20 +0530

    Product Type API Client added

---
+++ src/app/pages/productType/productType.routing.ts
@@ -0,0 +1,21 @@
+import { Routes, RouterModule }  from '@angular/router';
+
+import { ProductType } from './productType.component';
+import { ProductTypeForm } from './components/productTypeForm/productTypeForm.component';
+import { ProductTypeTable } from './components/productTypeTable/productTypeTable.component';
+import { ModuleWithProviders } from '@angular/core';
+
+// noinspection TypeScriptValidateTypes
+export const routes: Routes = [
+  {
+    path: '',
+    component: ProductType,
+     children: [
+      { path: 'form', component: ProductTypeForm },
+      { path: 'form/:id', component: ProductTypeForm },
+      { path: 'table', component: ProductTypeTable }
+    ]
+  }
+];
+
+export const routing = RouterModule.forChild(routes);
