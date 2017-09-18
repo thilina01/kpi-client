c651028d9abdfb4b5c671572fbc401d6c7acd323
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-09-18 10:18:10 +0530

    Organization API  client added

---
+++ src/app/pages/organization/organization.routing.ts
@@ -0,0 +1,21 @@
+import { Routes, RouterModule }  from '@angular/router';
+
+import { Organization } from './organization.component';
+import { OrganizationForm } from './components/organizationForm/organizationForm.component';
+import { OrganizationTable } from './components/organizationTable/organizationTable.component';
+import { ModuleWithProviders } from '@angular/core';
+
+// noinspection TypeScriptValidateTypes
+export const routes: Routes = [
+  {
+    path: '',
+    component: Organization,
+     children: [
+      { path: 'form', component: OrganizationForm },
+      { path: 'form/:id', component: OrganizationForm },
+      { path: 'table', component: OrganizationTable }
+    ]
+  }
+];
+
+export const routing = RouterModule.forChild(routes);
