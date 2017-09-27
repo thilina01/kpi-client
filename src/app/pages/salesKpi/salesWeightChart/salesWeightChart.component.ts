d556fe5e61b3954e7351699754f4bf0d6eaa941d
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-09-27 13:56:15 +0530

    Sales Weight Chart data in a table

--- src/app/pages/salesKpi/salesWeightChart/salesWeightChart.component.ts
+++ src/app/pages/salesKpi/salesWeightChart/salesWeightChart.component.ts
@@ -29,6 +29,7 @@ export class SalesWeightChart {
     this.chartService.getMonthlySalesWeight(startDateText, endDateText).subscribe((data) => {
       this.amChart.dataProvider = data.json();
       this.amChart.validateData();
+      this.chartService.fillTable(this.amChart);
     });
   }
 
