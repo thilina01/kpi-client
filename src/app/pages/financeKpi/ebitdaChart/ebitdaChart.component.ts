a4a04ed1eea92c287321e75581ae05c8bf63a385
Thilina Ranathunga <r.thilina@gmail.com>
2017-09-27 10:25:09 +0530

    Display chart data in a table

--- src/app/pages/financeKpi/ebitdaChart/ebitdaChart.component.ts
+++ src/app/pages/financeKpi/ebitdaChart/ebitdaChart.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-
+import { ChartService } from '../../chart/chart.service';
 import { EbitdaChartService } from './ebitdaChart.service';
-
-
 import 'style-loader!./ebitdaChart.scss';
-import { ChartService } from "../../chart/chart.service";
 
 @Component({
   selector: 'ebitda-chart',
@@ -17,22 +14,60 @@ export class EbitdaChart {
 
   constructor(private _ebitdaChartService: EbitdaChartService, private chartService: ChartService) {
     this.chartData = this._ebitdaChartService.getChartData([]);
-    var startDate = new Date();
+    let startDate = new Date();
     startDate.setMonth(startDate.getMonth() - 6);
-    var monthText :string;
-    monthText = ((startDate.getMonth() + 1) < 10 ? "0" + (startDate.getMonth() + 1) : (startDate.getMonth() + 1)) + "";
-    var startDateText = startDate.getFullYear()+"-"+monthText.slice(-2)+"-01";
+    let monthText: string;
+    monthText = ((startDate.getMonth() + 1) < 10 ? '0' + (startDate.getMonth() + 1) : (startDate.getMonth() + 1)) + '';
+    let startDateText = startDate.getFullYear() + '-' + monthText.slice(-2) + '-01';
 
-    var endDate = new Date();
-    var endDateText = endDate.getFullYear()+"-"+(endDate.getMonth()<10?"0"+endDate.getMonth():endDate.getMonth())+"-"+(new Date(endDate.getFullYear(), endDate.getMonth(), 0).getDate());
+    let endDate = new Date();
+    let endDateText = endDate.getFullYear() +
+      '-' + (endDate.getMonth() < 10 ? '0' + endDate.getMonth() : endDate.getMonth()) +
+      '-' + (new Date(endDate.getFullYear(), endDate.getMonth(), 0).getDate());
 
     this.chartService.getMonthlyEbitda(startDateText, endDateText).subscribe((data) => {
       this.amChart.dataProvider = data.json();
       this.amChart.validateData();
+      this.fillTable(this.amChart);
     });
   }
 
   initChart(chart: any) {
     this.amChart = chart;
   }
+
+  fillTable(chart: any) {
+
+    // check if export to table is enabled
+    if (chart.dataTableId === undefined)
+      return;
+
+    // get chart data
+    let data = chart.dataProvider;
+
+    // create a table
+    let holder = document.getElementById(chart.dataTableId);
+    let table = document.createElement('table');
+    holder.appendChild(table);
+    let tr, td;
+
+    // construct table
+    for (let i = 0; i < chart.graphs.length; i++) {
+
+      // add rows
+      tr = document.createElement('tr');
+      tr.setAttribute('data-valuefield', chart.graphs[i].valueField);
+      table.appendChild(tr);
+      td = document.createElement('td');
+      td.className = 'row-title';
+      td.innerHTML = chart.graphs[i].title;
+      tr.appendChild(td);
+
+      for (let x = 0; x < chart.dataProvider.length; x++) {
+        td = document.createElement('td');
+        td.innerHTML = chart.dataProvider[x][chart.graphs[i].valueField];
+        tr.appendChild(td);
+      }
+    }
+  };
 }
