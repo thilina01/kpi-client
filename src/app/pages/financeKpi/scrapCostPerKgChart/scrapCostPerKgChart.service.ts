a6c26ef7d57e8d443b45a2626911c12aa1a32471
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-09-27 12:03:48 +0530

    Display Scrap Cost Per Kg Chart data in a table

--- src/app/pages/financeKpi/scrapCostPerKgChart/scrapCostPerKgChart.service.ts
+++ src/app/pages/financeKpi/scrapCostPerKgChart/scrapCostPerKgChart.service.ts
@@ -17,14 +17,7 @@ export class ScrapCostPerKgChartService {
       'theme': 'blur',
       'depth3D': 20,
       'angle': 30,
-      'dataProvider': data/*[
-        { 'month': 'Nov-16', 'value': 90 },
-        { 'month': 'Dec-16', 'value': 95 },
-        { 'month': 'Jan-17', 'value': 94 },
-        { 'month': 'Feb-17', 'value': 88 },
-        { 'month': 'Mar-17', 'value': 93 },
-        { 'month': 'Apr-17', 'value': 88 }
-      ]*/,
+      'dataProvider': data,
       creditsPosition: 'top-right',
       'valueAxes': [{
         minVerticalGap: 50,
@@ -69,7 +62,12 @@ export class ScrapCostPerKgChartService {
         'position': 'top',
         'valueWidth':100,
         'valueAlign':'left'
-      }
+      },
+      'dataTableId': 'scrapCostPerKgchartdata',
+      'autoMargins': false,
+      'marginLeft': 100,
+      'marginRight': 0,
+      'marginBottom': 25
     };
   };
 }
