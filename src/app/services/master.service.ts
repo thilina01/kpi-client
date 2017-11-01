249879ecca047551219064fd10a1176f557a8e43
Thilina Ranathunga <r.thilina@gmail.com>
2017-11-01 10:32:17 +0530

    saveMany function added to handle data imports

--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -1,67 +1,73 @@
-import { Injectable, Inject } from '@angular/core';
+import {Injectable, Inject} from '@angular/core';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
-import { IAppConfig, APP_CONFIG } from '../app.config';
-import { AuthService } from './auth.service';
+import {IAppConfig, APP_CONFIG} from '../app.config';
+import {AuthService} from './auth.service';
 
 export class MasterService {
-    public getJsonHeaders(): HttpHeaders {
-        return new HttpHeaders({
-            'Content-Type': 'application/json',
-            'email': this.authService.email
-        });
-    };
-    private headers: Headers; // = new Headers({ 'Content-Type': 'application/json' });
-    public apiUrl: string;  // URL to web api
+  public getJsonHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      'email': this.authService.email
+    });
+  };
 
-    constructor(
-        public http: HttpClient,
-        private config: IAppConfig,
-        private authService: AuthService) {
-    }
+  private headers: Headers; // = new Headers({ 'Content-Type': 'application/json' });
+  public apiUrl: string;  // URL to web api
 
-    setApiUrl(path: string) {
-        this.apiUrl = this.config.apiEndpoint + path;
-    }
+  constructor(public http: HttpClient,
+              private config: IAppConfig,
+              private authService: AuthService) {
+  }
 
-    getAll(): Observable<any> {
-        return this.http.get(this.apiUrl, { headers: this.getJsonHeaders() })
-            .catch(err => this.handleError(err));
-    }
+  setApiUrl(path: string) {
+    this.apiUrl = this.config.apiEndpoint + path;
+  }
 
-    getPage(page, size): Observable<any> {
-        return this.http.get(this.apiUrl + 'page?page=' + page + '&size=' + size, { headers: this.getJsonHeaders() })
-            .catch(err => this.handleError(err));
-    }
+  getAll(): Observable<any> {
+    return this.http.get(this.apiUrl, {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
 
-    getCombo(): Observable<any> {
-        return this.http.get(this.apiUrl + 'combo', { headers: this.getJsonHeaders() })
-            .catch(err => this.handleError(err));
-    }
+  getPage(page, size): Observable<any> {
+    return this.http.get(this.apiUrl + 'page?page=' + page + '&size=' + size, {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
 
-    getOne(id: number): Observable<any> {
-        return this.http.get(this.apiUrl + id, { headers: this.getJsonHeaders() })
-            .catch(err => this.handleError(err));
-    }
+  getCombo(): Observable<any> {
+    return this.http.get(this.apiUrl + 'combo', {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
 
-    save(object: Object): Observable<any> {
-        return this.http
-            .post(this.apiUrl, JSON.stringify(object), { headers: this.getJsonHeaders() })
-            .catch(err => this.handleError(err));
-    }
+  getOne(id: number): Observable<any> {
+    return this.http.get(this.apiUrl + id, {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
 
-    delete(id: number): Observable<any> {
-        return this.http
-            .delete(this.apiUrl + id, { headers: this.getJsonHeaders() })
-            .catch(err => this.handleError(err));
-    }
+  save(object: Object): Observable<any> {
+    return this.http
+      .post(this.apiUrl, JSON.stringify(object), {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
 
-    public handleError(httpErrorResponse: any): Observable<any> {
-        console.error('An error occurred'); // for demo purposes only    
-        console.log(httpErrorResponse)
-        alert(httpErrorResponse.error.message);
-        return Observable.empty<Response>();
-    }
+  saveMany(objects: Object[]): Observable<any> {
+    return this.http
+      .post(this.apiUrl+'many', JSON.stringify(objects), {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http
+      .delete(this.apiUrl + id, {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
+
+  public handleError(httpErrorResponse: any): Observable<any> {
+    console.error('An error occurred'); // for demo purposes only
+    console.log(httpErrorResponse)
+    alert(httpErrorResponse.error.message);
+    return Observable.empty<Response>();
+  }
 }
