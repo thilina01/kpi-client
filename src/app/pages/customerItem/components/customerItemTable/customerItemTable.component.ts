078983989a82bbcb239793949b40b01d0dc9aa62
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2017-11-09 15:52:09 +0530

    Customer Item Form filtered by customer

--- src/app/pages/customerItem/components/customerItemTable/customerItemTable.component.ts
+++ src/app/pages/customerItem/components/customerItemTable/customerItemTable.component.ts
@@ -1,9 +1,10 @@
 
 import { SharedService } from '../../../../services/shared.service';
-import { Component, ViewEncapsulation } from '@angular/core';
-import { ConfirmationService, Message } from 'primeng/primeng';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { ConfirmationService, Message, DataTable } from 'primeng/primeng';
 import { Router } from '@angular/router';
 import { CustomerItemService } from '../../customerItem.service';
+import { CustomerService } from '../../../customer/customer.service';
 
 @Component({
   selector: 'customer-item-table',
@@ -14,29 +15,57 @@ import { CustomerItemService } from '../../customerItem.service';
 
 export class CustomerItemTable {
   rows = [];
+  [x: string]: any;
   timeout: any;
+  customerList: any;
+  filteredCustomers: any[];
+  customers: any;
   totalRecords: number;
+  customer: any = { id: 0, 'code': 'ALL', 'name': '', 'display': 'All' }
+  @ViewChild(DataTable) dataTable: DataTable;
 
   constructor(protected service: CustomerItemService,
     private router: Router,
     private confirmationService: ConfirmationService,
+    private customerService: CustomerService,
     private sharedService: SharedService) {
+    this.getCustomers();
     this.loadData()
+  }
+  getCustomers(): void {
+    this.customerService.getCombo().subscribe(customers => {
+      this.customers = customers;
+      this.customers.unshift({ id: 0, 'code': 'ALL', 'name': '' , 'display': 'All' });
+    });
   }
 
   loadData() {
-    this.service.getPage(0, 20).subscribe((data: any) => {
-      this.rows = data.content;
-      this.totalRecords = data.totalElements;
-    });
+    if (this.customer.id != undefined && this.customer.id != 0) {
+      this.service.getPageByCustomer(this.customer, 0, 20).subscribe((data: any) => {
+        this.rows = data.content;
+        this.totalRecords = data.totalElements;
+      });
+    } else {
+      this.service.getPage(0, 20).subscribe((data: any) => {
+        this.rows = data.content;
+        this.totalRecords = data.totalElements;
+      });
+    }
   }
 
   lazy(event: any, table: any) {
     const search = table.globalFilter ? table.globalFilter.value : null;
-    this.service.getPage((event.first / event.rows), event.rows).subscribe((data: any) => {
-      this.rows = data.content;
-      this.totalRecords = data.totalElements;
-    });
+    if (this.customer.id != undefined && this.customer.id != 0) {
+      this.service.getPageByCustomer(this.customer, (event.first / event.rows), event.rows).subscribe((data: any) => {
+        this.rows = data.content;
+        this.totalRecords = data.totalElements;
+      });
+    } else {
+      this.service.getPage((event.first / event.rows), event.rows).subscribe((data: any) => {
+        this.rows = data.content;
+        this.totalRecords = data.totalElements;
+      });
+    }
   }
 
   onPage(event) {
@@ -66,4 +95,24 @@ export class CustomerItemTable {
       }
     });
   }
+  /*================== Customer Filter ===================*/
+  filterCustomers(event) {
+    let query = event.query.toLowerCase();
+    this.filteredCustomers = [];
+    for (let i = 0; i < this.customers.length; i++) {
+      let customer = this.customers[i];
+      if (customer.code.toLowerCase().indexOf(query) == 0) {
+        this.filteredCustomers.push(customer);
+      }
+    }
+  }
+
+  onCustomerSelect(customer: any) {
+    this.customer = customer;
+    this.loadData();
+  }
+
+  /*================== End Of Customer Filter ===================*/
 }
+
+
