4b236d469a9fe6bd6a1918ae536608c26ce40ff0
Thilina Ranathunga <r.thilina@gmail.com>
2017-12-08 12:45:47 +0530

    code improvements

--- src/app/pages/country/components/countryForm/countryForm.component.ts
+++ src/app/pages/country/components/countryForm/countryForm.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewEncapsulation, Input } from '@angular/core';
-import { Subscription } from 'rxjs/Subscription';
 import { ActivatedRoute, Params, Router } from '@angular/router'
-import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import {  AbstractControl, Validators } from '@angular/forms';
 
 import { SharedService } from '../../../../services/shared.service';
 import { CountryService } from '../../country.service';
@@ -15,59 +14,27 @@ import { CountryService } from '../../country.service';
 export class CountryForm {
     JSON: any = JSON;
 
-    public formGroup: FormGroup;
     country: any = {};
-    subscription: Subscription;
 
     constructor(protected service: CountryService,
         private route: ActivatedRoute,
         private router: Router,
-        fb: FormBuilder,
         private sharedService: SharedService) {
-        this.formGroup = fb.group({
-            id: '',
-            code: ['', Validators.required],
-            name: ['', Validators.required]
-        });
     }
 
     ngOnInit(): void {
-        this.route.params.subscribe(
-            (params: Params) => {
-                let id = params['id'];
-                id = id == undefined ? '0' : id;
-                if (id != '0') {
-                    this.service.getOne(+id).subscribe(
-                        (data) => {
-                            this.loadForm(data);
-                        }
-                    )
-                }
-            }
-        );
-    }
-
-    loadForm(data: any) {
-        if (data != null) {
-            this.country = data;
+        let id = this.route.snapshot.paramMap.get('id');
+        if (id) {
+          this.service.getOne(+id).subscribe(country => { if (country) this.country = country; } );
         }
-        this.formGroup.patchValue(this.country, { onlySelf: true });
     }
 
-    public onSubmit(values: any, event: Event): void {
-        event.preventDefault();
-        console.log(values);
-        this.service.save(values).subscribe(
+    public save(country: any): void {
+        this.service.save(country).subscribe(
             (data) => {
                 this.sharedService.addMessage({ severity: 'info', summary: 'Success', detail: 'Operation Success' });
-                this.resetForm();
                 this.router.navigate(['/pages/country/form/']);
             }
         );
     }
-
-    public resetForm() {
-        this.formGroup.reset();
-    }
-
 }
