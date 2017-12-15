603cebe70cd613e1f6b12b588ecfc9f2c2ada75a
Thilina Ranathunga <r.thilina@gmail.com>
2017-12-15 12:33:06 +0530

    added new api endpoint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,16 @@ export interface IAppConfig {
   apiEndpoint: string;
 }
 let hostname = location.hostname;
-let apiEndpoint = (hostname === 'kpi.trwlanka.com' || hostname === 'otr-kpi.firebaseapp.com' || hostname === 'otr-kpi.nanosl.com') ? 'http://tmsapi.trwlanka.com/' : hostname === '192.168.1.171' ? 'http://' + hostname + ':8080/file-manager-api/' : 'http://' + hostname + ':8080/';
+let apiEndpoint =
+  hostname === 'kpi.trwlanka.com' ||
+  hostname === 'otr-kpi.firebaseapp.com' ||
+  hostname === 'otr-kpi.nanosl.com' ||
+  hostname === 'kpi.otrlanka.com'
+    ? 'http://api.otrlanka.com/'
+    : hostname === '192.168.1.171'
+      ? 'http://' + hostname + ':8080/file-manager-api/'
+      : 'http://' + hostname + ':8080/';
 
 export const AppConfig: IAppConfig = {
-
-  apiEndpoint: apiEndpoint,
-
+  apiEndpoint: apiEndpoint
 };
