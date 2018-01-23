c9b2b8aa48489dbd8cc6cab756b1e12ae417ef61
Thilina Ranathunga <r.thilina@gmail.com>
2018-01-23 14:35:44 +0530

    Fill form on enter disable id after fill skip filling date if null code cleanup

--- src/app/pages/dispatchRelease/components/dispatchReleaseForm/dispatchReleaseForm.component.ts
+++ src/app/pages/dispatchRelease/components/dispatchReleaseForm/dispatchReleaseForm.component.ts
@@ -10,108 +10,91 @@ import { DispatchService } from '../../../../services/dispatch.service';
 import 'rxjs/add/operator/take';
 
 @Component({
-    selector: 'dispatch-release-form',
-    encapsulation: ViewEncapsulation.None,
-    styleUrls: ['./dispatchReleaseForm.scss'],
-    templateUrl: './dispatchReleaseForm.html',
+  selector: 'dispatch-release-form',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./dispatchReleaseForm.scss'],
+  templateUrl: './dispatchReleaseForm.html',
 
 })
 export class DispatchReleaseForm {
 
-    @Input('formGroup')
-    public formGroup: FormGroup;
-    @ViewChild(DataTable) dataTableComponent: DataTable;
-    public dispatchNoteFormGroup: FormGroup;
-    dataTable: any;
-    JSON: any = JSON;
+  public formGroup: FormGroup;
+  @ViewChild(DataTable) dataTableComponent: DataTable;
+  JSON: any = JSON;
+  idTextField: HTMLInputElement;
+  dispatchNote: any = {};
 
-    public FormGroup: FormGroup;
-    dispatchRelease: any = {};
-    subscription: Subscription;
-
-    dispatchNote: any = {};
-    dispatchReleaseTime: Date;
-    totalRecords: number;
-    rows = [];
-    dispatchNoteList = [];
-    dispatchList = [];
-
-    constructor(protected service: DispatchNoteService,
-        private route: ActivatedRoute,
-        private router: Router,
-        fb: FormBuilder,
-        private confirmationService: ConfirmationService,
-        private dispatchService: DispatchService,
-        private sharedService: SharedService) {
-
-        this.formGroup = fb.group({
-            id: '',
-            vehicleNumber: '',
-            dispatchReleaseTime: [this.dispatchReleaseTime, Validators.required],
-            containerNumber: '',
-            recipient: ''
-        });
-    }
-    ngOnInit(): void {
-        this.route.params.subscribe(
-            (params: Params) => {
-                let id = params['id'];
-                id = id == undefined ? '0' : id;
-                if (id != '0') {
-                    this.loadForm(id);
-                }
-            }
-        );
-    }
-
-    fill(): void {
-        this.clear();
-        let id = this.formGroup.value.id;
-        if (id == undefined || id == '') { return; }
-        this.loadForm(id);
-    }
-
-    clear(): void {
-        this.dispatchNote = {};
-        if (this.dataTableComponent != undefined) {
-            this.dataTableComponent.reset();
+  constructor(protected service: DispatchNoteService,
+    private route: ActivatedRoute,
+    private router: Router,
+    fb: FormBuilder,
+    private confirmationService: ConfirmationService,
+    private dispatchService: DispatchService,
+    private sharedService: SharedService) {
+    this.formGroup = fb.group({
+      id: '',
+      vehicleNumber: '',
+      dispatchReleaseTime: [null, Validators.required],
+      containerNumber: '',
+      recipient: ''
+    });
+  }
+  ngOnInit(): void {
+    this.idTextField = <HTMLInputElement> document.getElementById('id');
+    this.route.params.subscribe(
+      (params: Params) => {
+        let id = params['id'];
+        id = id === undefined ? '0' : id;
+        if (id !== '0') {
+          this.loadForm(id);
         }
+      }
+    );
+  }
 
+  fill(): void {
+    this.clear();
+    let id = this.formGroup.value.id;
+    if (id === undefined || id === '') { return; }
+    this.loadForm(id);
+  }
+
+  clear(): void {
+    this.dispatchNote = {};
+    if (this.dataTableComponent !== undefined) {
+      this.dataTableComponent.reset();
     }
+  }
 
-    keyDown(event) {
-        if (event.keyCode == 13) {
-            this.clear();
-            this.fill();
+  loadForm(id: any) {
+    this.service.get(+id).take(1).subscribe(
+      (data) => {
+        if (data != null) {
+          if (data.dispatchReleaseTime !== null) {
+            data.dispatchReleaseTime = new Date(data.dispatchReleaseTime);
+          }
+          this.dispatchNote = data;
+          this.idTextField.disabled = true;
         }
-    }
+        this.formGroup.patchValue(this.dispatchNote, { onlySelf: true });
+      }
+    );
+  }
 
-    loadForm(id: any) {
-        this.service.get(+id).take(1).subscribe(
-            (data) => {
-                if (data != null) {
-                    data.dispatchReleaseTime = new Date(data.dispatchReleaseTime);
-                    this.dispatchNote = data;
-                }
-                this.formGroup.patchValue(this.dispatchNote, { onlySelf: true });
-            }
-        )
-    }
+  public onSubmit(values: any, event: Event): void {
+    event.preventDefault();
+    this.service.saveRelease(values).subscribe(
+      (data) => {
+        this.sharedService.addMessage({ severity: 'info', summary: 'Success', detail: 'Operation Success' });
+        this.resetForm();
+      }
+    );
+  }
 
-    public onSubmit(values: any, event: Event): void {
-        event.preventDefault();
-        console.log(values);
-        this.service.saveRelease(values).subscribe(
-            (data) => {
-                this.sharedService.addMessage({ severity: 'info', summary: 'Success', detail: 'Operation Success' });
-                this.resetForm();
-            }
-        );
-    }
+  public resetForm() {
+    this.idTextField.disabled = false;
+    this.formGroup.reset();
+    this.fill();
 
-    public resetForm() {
-        this.formGroup.reset();
-        this.fill();
-
-    }
+  }
 }
