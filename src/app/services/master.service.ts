ab1321da4fbbf93cfe5dd7ad16ccb2e0d5b63733
Thilina Ranathunga <r.thilina@gmail.com>
2018-02-02 16:02:19 +0530

    dependency changes to fix problem in angular http client when parsing 200 ok response

--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -1,12 +1,17 @@
-import {Injectable, Inject} from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
-import {IAppConfig, APP_CONFIG} from '../app.config';
-import {AuthService} from './auth.service';
+import { empty } from 'rxjs/observable/empty';
+import { IAppConfig, APP_CONFIG } from '../app.config';
+import { AuthService } from './auth.service';
 
 export class MasterService {
+  public apiUrl: string;  // URL to web api
+  public endPoint: string;  // URL to web api
+  private headers: Headers; // = new Headers({ 'Content-Type': 'application/json' });
+
   public getJsonHeaders(): HttpHeaders {
     return new HttpHeaders({
       'Content-Type': 'application/json',
@@ -14,18 +19,14 @@ export class MasterService {
     });
   };
 
-  private headers: Headers; // = new Headers({ 'Content-Type': 'application/json' });
-  public apiUrl: string;  // URL to web api
-  public endPoint: string;  // URL to web api
-  
   constructor(public http: HttpClient,
-              private config: IAppConfig,
-              private authService: AuthService) {
+    private config: IAppConfig,
+    private authService: AuthService) {
   }
 
   setEndPoint(endPoint: string) {
     this.endPoint = endPoint;
-    this.setApiUrl(endPoint+'/')
+    this.setApiUrl(endPoint + '/');
   }
 
   setApiUrl(path: string) {
@@ -33,47 +34,47 @@ export class MasterService {
   }
 
   getAll(): Observable<any> {
-    return this.http.get(this.apiUrl, {headers: this.getJsonHeaders()})
+    return this.http.get(this.apiUrl, { headers: this.getJsonHeaders() })
       .catch(err => this.handleError(err));
   }
 
   getPage(page, size): Observable<any> {
-    return this.http.get(this.apiUrl + 'page?page=' + page + '&size=' + size, {headers: this.getJsonHeaders()})
+    return this.http.get(this.apiUrl + 'page?page=' + page + '&size=' + size, { headers: this.getJsonHeaders() })
       .catch(err => this.handleError(err));
   }
 
   getCombo(): Observable<any> {
-    return this.http.get(this.apiUrl + 'combo', {headers: this.getJsonHeaders()})
+    return this.http.get(this.apiUrl + 'combo', { headers: this.getJsonHeaders() })
       .catch(err => this.handleError(err));
   }
 
   get(id: number): Observable<any> {
-    return this.http.get(this.apiUrl + id, {headers: this.getJsonHeaders()})
+    return this.http.get(this.apiUrl + id, { headers: this.getJsonHeaders() })
       .catch(err => this.handleError(err));
   }
 
   save(object: Object): Observable<any> {
     return this.http
-      .post(this.apiUrl, JSON.stringify(object), {headers: this.getJsonHeaders()})
+      .post(this.apiUrl, JSON.stringify(object), { headers: this.getJsonHeaders() })
       .catch(err => this.handleError(err));
   }
 
   saveMany(objects: Object[]): Observable<any> {
     return this.http
-      .post(this.apiUrl+'many', JSON.stringify(objects), {headers: this.getJsonHeaders()})
+      .post(this.apiUrl + 'many', JSON.stringify(objects), { headers: this.getJsonHeaders() })
       .catch(err => this.handleError(err));
   }
 
   delete(id: number): Observable<any> {
     return this.http
-      .delete(this.apiUrl + id, {headers: this.getJsonHeaders()})
+      .delete(this.apiUrl + id, { headers: this.getJsonHeaders() })
       .catch(err => this.handleError(err));
   }
 
   public handleError(httpErrorResponse: any): Observable<any> {
     console.error('An error occurred'); // for demo purposes only
-    console.log(httpErrorResponse)
+    console.log(httpErrorResponse);
     alert(httpErrorResponse.error.message);
-    return Observable.empty<Response>();
+    return empty();
   }
 }
