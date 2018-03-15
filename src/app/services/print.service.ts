d0bef33180da345d5649524c5e23be1b1f95f8f7
Thilina Ranathunga <r.thilina@gmail.com>
2018-03-15 11:51:07 +0530

    fix: Margins and incorrect page break
    bootstrap updated to V4

--- src/app/services/print.service.ts
+++ src/app/services/print.service.ts
@@ -25,61 +25,78 @@ export class PrintService {
     popupWin = window.open('', '_blank', 'top=15,left=10,height=100%,width=auto');
     popupWin.document.open();
     popupWin.document.write(`
-  <html>
-    <head>
-      <title>Print tab</title>
-<link rel='stylesheet'  type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' media='print'>
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
-<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
-
-      <style>
-      .table-condensed{
-font-size: 9px;
-}
-.table > tbody > tr > td {
- vertical-align: middle;
-}
-      @media print {
-
-hr {
-display: block;
-height: 1px;
-border: 0;
-border-top: 1px solid #000;
-margin: 1em 0;
-padding: 0;
-}
-div.divFooter {
-page-break-inside: avoid;
-bottom: 0;
-font-size: 10px;
-margin-bottom: 1em;
-}
-
-@page {
-size: ${width} ${height};
-}
-
-html, body {
-width: 1024px;
-
-font-size: 1em !important;
-color: #000 !important;
-font-family: Arial !important;
-}
-
-body {
-margin: 0 auto;
-}
-
-}
-
-.pagebreak { page-break-before: always; } /* page-break-after works, as well */
-      </style>
-    </head>
-<body onload='window.print();window.close()'>${printContents}</body>
-  </html>`
-    );
+    <html>
+      <head>
+          <title>Print tab</title>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+                  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+                  crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+                  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+                  crossorigin="anonymous"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+                  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+                  crossorigin="anonymous"></script>
+      
+          <style>
+              .table-condensed {
+                  font-size: 9px;
+              }
+      
+              .table > tbody > tr > td {
+                  vertical-align: middle;
+              }
+      
+              @media print {
+      
+                  hr {
+                      display: block;
+                      height: 1px;
+                      border: 0;
+                      border-top: 1px solid #000;
+                      margin: 1em 0;
+                      padding: 0;
+                  }
+      
+                  div.divFooter {
+                      page-break-inside: avoid;
+                      bottom: 0;
+                      font-size: 10px;
+                      margin-bottom: 1em;
+                  }
+      
+                  @page {
+                      size: ${width} ${height};
+                      margin-left: 0.75in;
+                      /* margin: 25mm 25mm 25mm 25mm;  */
+                  }
+      
+                  html, body {
+                      width: 1024px;
+      
+                      font-size: 1em !important;
+                      color: #000 !important;
+                      font-family: Arial !important;
+                  }
+      
+                  body {
+                      margin: 0 auto;
+                  }
+      
+              }
+      
+              .pagebreak {
+                  page-break-before: always;
+              }
+      
+              /* page-break-after works, as well */
+          </style>
+      </head>
+      <body onload='window.print();window.close()'>${printContents}</body>
+    </html>
+    `);
     popupWin.document.close();
   }
 
