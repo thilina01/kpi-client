25aec70911a2d3c94626355e2e3761d9d6aa59ae
Umesha <umesha.nipunialawatta@gmail.com>
2018-06-26 10:56:41 +0530

    Add section and date duration serach in scrap table

--- src/app/pages/scrap/components/scrapTable/scrapTable.component.ts
+++ src/app/pages/scrap/components/scrapTable/scrapTable.component.ts
@@ -1,49 +1,86 @@
-
 import { SharedService } from '../../../../services/shared.service';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { ConfirmationService, Message } from 'primeng/primeng';
 import { Router } from '@angular/router';
 import { ScrapService } from '../../scrap.service';
+import { SectionService } from '../../../section/section.service';
 
 @Component({
   selector: 'scrap-table',
   encapsulation: ViewEncapsulation.None,
   styleUrls: ['./scrapTable.scss'],
-  templateUrl: './scrapTable.html',
+  templateUrl: './scrapTable.html'
 })
-
 export class ScrapTable {
   rows = [];
   timeout: any;
   totalRecords: number;
+  startDate: Date;
+  endDate: Date;
+  sections: any;
+  section: any = { id: 0, code: 'ALL', display: 'All Sections' };
+  pageSize = 20;
 
-  constructor(protected service: ScrapService,
+  constructor(
+    protected service: ScrapService,
     private router: Router,
     private confirmationService: ConfirmationService,
-    private sharedService: SharedService) {
-    this.loadData()
+    private sectionService: SectionService,
+    private sharedService: SharedService
+  ) {
+    this.loadData();
+    this.getSections();
+  }
+
+  getSections(): void {
+    this.sectionService.getCombo().subscribe(sections => {
+      this.sections = sections;
+      this.sections.unshift({ id: 0, code: 'ALL', display: 'All Sections' });
+    });
   }
 
   loadData() {
-    this.service.getPage(0, 20).subscribe((data: any) => {
-      this.rows = data.content;
-      this.totalRecords = data.totalElements;
-    });
+    this.service
+      .getSectionAndScrapDateBetweenPage(0, '1970-01-01', '2100-12-31', 0, 20)
+      .subscribe((data: any) => {
+        this.fillTable(data);
+      });
   }
 
   lazy(event: any, table: any) {
-    const search = table.globalFilter ? table.globalFilter.value : null;
-    this.service.getPage((event.first / event.rows), event.rows).subscribe((data: any) => {
-      this.rows = data.content;
-      this.totalRecords = data.totalElements;
-    });
+    console.log(event);
+    this.search(event.first / event.rows, event.rows);
+  }
+
+  search(first: number, pageSize: number): void {
+    pageSize = pageSize === undefined ? this.pageSize : pageSize;
+    this.service
+      .getSectionAndScrapDateBetweenPage(
+        this.section !== undefined ? this.section.id : 0,
+        this.startDate === undefined
+          ? '1970-01-01'
+          : this.sharedService.YYYYMMDD(this.startDate),
+        this.endDate === undefined
+          ? '2100-12-31'
+          : this.sharedService.YYYYMMDD(this.endDate),
+        first,
+        pageSize
+      )
+      .subscribe((data: any) => {
+        this.fillTable(data);
+      });
   }
 
   onPage(event) {
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
-      console.log('paged!', event);
+      this.search(event.first, event.rows);
     }, 100);
+  }
+
+  fillTable(data: any) {
+    this.rows = data.content;
+    this.totalRecords = data.totalElements;
   }
 
   onRowDblclick(data: any): void {
@@ -59,11 +96,31 @@ export class ScrapTable {
       message: 'Are you sure that you want to Delete?',
       accept: () => {
         this.service.delete(id).subscribe(response => {
-          this.sharedService.addMessage({ severity: 'info', summary: 'Deleted', detail: 'Delete success' });
-          this.loadData()
-        }
-        );
+          this.sharedService.addMessage({
+            severity: 'info',
+            summary: 'Deleted',
+            detail: 'Delete success'
+          });
+          this.loadData();
+        });
       }
     });
   }
+
+  /*================== Section Filter ===================*/
+  filteredSections: any[];
+  filterSections(event) {
+    let query = event.query.toLowerCase();
+    this.filteredSections = [];
+    for (let i = 0; i < this.sections.length; i++) {
+      let section = this.sections[i];
+      if (section.display.toLowerCase().indexOf(query) >= 0) {
+        this.filteredSections.push(section);
+      }
+    }
+  }
+  onSectionSelect(section: any) {
+    console.log(event);
+  }
+  /*================== End Of Section Filter ===================*/
 }
--- src/app/pages/scrap/scrap.service.ts
+++ src/app/pages/scrap/scrap.service.ts
@@ -3,6 +3,7 @@ import { MasterService } from '../../services/master.service';
 import { HttpClient } from '@angular/common/http';
 import { APP_CONFIG, IAppConfig } from '../../app.config';
 import { AuthService } from '../../services/auth.service';
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class ScrapService extends MasterService {
@@ -10,5 +11,10 @@ export class ScrapService extends MasterService {
   constructor(private anHttp: HttpClient, @Inject(APP_CONFIG) private aConfig: IAppConfig, private anAuthService: AuthService) {
     super(anHttp, aConfig, anAuthService);
     this.setApiUrl('scraps/');
+  }
+
+  getSectionAndScrapDateBetweenPage(section, startDate, endDate, page, size): Observable<any> {
+    return this.http.get(this.apiUrl + 'sectionAndScrapDateBetween?section=' + section+  '&startDate=' + startDate + '&endDate=' + endDate + '&page=' + page + '&size=' + size, { headers: this.getJsonHeaders() })
+      .catch(err => this.handleError(err));
   }
 }
