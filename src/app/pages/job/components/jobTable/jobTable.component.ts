7c7774ddf8cb9bbfd3f5536e218fb7f5d8912801
Umesha <umesha.nipunialawatta@gmail.com>
2018-07-04 11:16:48 +0530

    fix:search problem

--- src/app/pages/job/components/jobTable/jobTable.component.ts
+++ src/app/pages/job/components/jobTable/jobTable.component.ts
@@ -1,16 +1,16 @@
-import { SharedService } from "../../../../services/shared.service";
-import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
-import { ConfirmationService, Message } from "primeng/primeng";
-import { Router } from "@angular/router";
-import { JobService } from "../../job.service";
-import { DataTable } from "primeng/components/datatable/datatable";
-import { ItemService } from "../../../item/item.service";
+import { SharedService } from '../../../../services/shared.service';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { ConfirmationService, Message } from 'primeng/primeng';
+import { Router } from '@angular/router';
+import { JobService } from '../../job.service';
+import { DataTable } from 'primeng/components/datatable/datatable';
+import { ItemService } from '../../../item/item.service';
 
 @Component({
-  selector: "job-table",
+  selector: 'job-table',
   encapsulation: ViewEncapsulation.None,
-  styleUrls: ["./jobTable.scss"],
-  templateUrl: "./jobTable.html"
+  styleUrls: ['./jobTable.scss'],
+  templateUrl: './jobTable.html'
 })
 export class JobTable {
   filteredItems: any[];
@@ -22,77 +22,65 @@ export class JobTable {
   totalRecords: number;
   pageSize = 20;
   @ViewChild(DataTable) dataTable: DataTable;
-  item: any = { id: 0, code: "ALL", display: "All Items" };
+  item: any = { id: 0, code: 'ALL', display: 'All Items' };
 
   constructor(
     protected service: JobService,
     private router: Router,
     private confirmationService: ConfirmationService,
     private itemService: ItemService,
-    private sharedService: SharedService) {
+    private sharedService: SharedService
+  ) {
     this.loadData();
     this.getItems();
-  }
-
-  loadData() {
-    if (
-      this.jobNo != undefined &&
-      this.jobNo != 0 &&
-      this.item != undefined &&
-      this.item != 0
-    ) {
-      this.service.getItemPage(0, 0, 0, 20).subscribe((data: any) => {
-        this.fillTable(data);
-      });
-    } else {
-      this.service.getPage(0, 20).subscribe((data: any) => {
-        this.rows = data.content;
-        this.totalRecords = data.totalElements;
-      });
-    }
-  }
-
-  lazy(event: any, table: any) {
-    const search = table.globalFilter ? table.globalFilter.value : null;
-    if (
-      this.jobNo != undefined &&
-      this.jobNo != 0 &&
-      this.item != undefined &&
-      this.item != 0
-    ) {
-      this.service.getItemPage(0, 0, 0, 20).subscribe((data: any) => {
-        this.rows = data.content;
-        this.totalRecords = data.totalElements;
-      });
-    } else {
-      this.service
-        .getPage(event.first / event.rows, event.rows)
-        .subscribe((data: any) => {
-          this.rows = data.content;
-          this.totalRecords = data.totalElements;
-        });
-    }
   }
 
   getItems(): void {
     this.itemService.getCombo().subscribe(items => {
       this.items = items;
-      this.items.unshift({ id: 0, code: "ALL", display: "All Items" });
+      this.items.unshift({ id: 0, code: 'ALL', display: 'All Items' });
     });
+  }
+
+  loadData() {
+    this.service.getPage(0, 20).subscribe((data: any) => {
+      this.fillTable(data);
+    });
+  }
+
+  lazy(event: any, table: any) {
+    this.search(event.first / event.rows, event.rows);
   }
 
   search(first: number, pageSize: number): void {
     pageSize = pageSize === undefined ? this.pageSize : pageSize;
-    this.service
-      .getItemPage(
-        this.jobNo !== undefined ? this.jobNo : 0,
-        this.item !== undefined ? this.item.id : 0,
-        first,
-        pageSize
-      )
-      .subscribe((data: any) => {
+    if (this.item !== undefined ? this.item.id : 0) {
+      this.service
+        .getItemPage(
+          this.jobNo !== undefined ? this.jobNo : 0,
+          this.item !== undefined ? this.item.id : 0,
+          first,
+          pageSize
+        )
+        .subscribe((data: any) => {
+          this.fillTable(data);
+        });
+    } else if (this.jobNo !== undefined ? this.jobNo : 0) {
+      this.service
+        .getItemPage(
+          this.jobNo !== undefined ? this.jobNo : 0,
+          this.item !== undefined ? this.item.id : 0,
+          first,
+          pageSize
+        )
+        .subscribe((data: any) => {
+          this.fillTable(data);
+        });
+    } else {
+      this.service.getPage(first, pageSize).subscribe((data: any) => {
         this.fillTable(data);
       });
+    }
   }
 
   fillTable(data: any) {
@@ -103,27 +91,27 @@ export class JobTable {
   onPage(event) {
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
-      console.log("paged!", event);
+      console.log('paged!', event);
     }, 100);
   }
 
   onRowDblclick(data: any): void {
-    this.router.navigate(["/pages/job/form/" + data.id]);
+    this.router.navigate(['/pages/job/form/' + data.id]);
   }
 
   navigateToForm(id: any): void {
-    this.router.navigate(["/pages/job/form/" + id]);
+    this.router.navigate(['/pages/job/form/' + id]);
   }
 
   delete(id: number) {
     this.confirmationService.confirm({
-      message: "Are you sure that you want to Delete?",
+      message: 'Are you sure that you want to Delete?',
       accept: () => {
         this.service.delete(id).subscribe(response => {
           this.sharedService.addMessage({
-            severity: "info",
-            summary: "Deleted",
-            detail: "Delete success"
+            severity: 'info',
+            summary: 'Deleted',
+            detail: 'Delete success'
           });
           this.loadData();
         });
