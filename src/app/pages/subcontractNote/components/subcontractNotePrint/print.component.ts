137713992b6a88bc65c44131e445d6532358bfe1
Umesha <umesha.nipunialawatta@gmail.com>
2018-07-26 13:39:49 +0530

    change print format in subcontractNote

--- src/app/pages/subcontractNote/components/subcontractNotePrint/print.component.ts
+++ src/app/pages/subcontractNote/components/subcontractNotePrint/print.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input } from "@angular/core";
-import { SubcontractNoteService } from "../../subcontractNote.service";
-import { PrintService } from "../../../../services/print.service";
-import { OrganizationService } from "../../../organization/organization.service";
-import "rxjs/add/operator/take";
+
+
+
+
+import { Component, Input } from '@angular/core';
+import { SubcontractNoteService } from '../../subcontractNote.service';
+import { PrintService } from '../../../../services/print.service';
+import { OrganizationService } from '../../../organization/organization.service';
+import 'rxjs/add/operator/take';
 
 @Component({
-  selector: "print",
-  templateUrl: "./print.html"
+  selector: 'print',
+  templateUrl: './print.html'
 })
 export class Print {
   totalQuantity = 0.0;
   totalCost= 0.0;
-  subcontractOperationList = [];
-
   @Input()
   set id(id: number) {
     if (this.id !== 0) {
@@ -22,22 +24,37 @@ export class Print {
         .subscribe(data => {
           if (data === null) return;
           this.subcontractNote = data;
-          this.subcontractOperationList = [];
           this.totalQuantity = 0.0;
           this.totalCost = 0.0;
-
+          this.rowList = [];
 
           for (let i = 0; i < this.subcontractNote.subcontractOperationList.length; i++) {
             let subcontractOperation = this.subcontractNote.subcontractOperationList[i];
+            let jobId = subcontractOperation.job.id ;
+            let productTypeId = subcontractOperation.subcontractOperationRate.subcontractorOperation.subcontractOperationDefinition.productType.id;
 
-            subcontractOperation.quantity = subcontractOperation.quantity;
-            this.totalQuantity += subcontractOperation.quantity;
+            if (this.rowList.length === 0){
+              this.totalQuantity += subcontractOperation.quantity;
+              this.rowList.push(this.makeRow(jobId, productTypeId, subcontractOperation));
+            } else {
+              let found = false;
+              for (let row of this.rowList) {
+                if (row.jobId === jobId && row.productTypeId === productTypeId ){
+                  found = true;
+                  row.subRowList.push(this.makeSubRow(subcontractOperation));
+                }
+              }
+              if (!found){
+                this.totalQuantity += subcontractOperation.quantity;
+                this.rowList.push(this.makeRow(jobId, productTypeId, subcontractOperation));
+              }
+            }
 
-            this.totalCost +=subcontractOperation.quantity * subcontractOperation.subcontractOperationRate.rate;
-
+            this.totalCost += subcontractOperation.quantity * subcontractOperation.subcontractOperationRate.rate;
           }
+
           setTimeout(() => {
-            let element = document.getElementById("print");
+            let element = document.getElementById('print');
             if (element != null) {
               this.printService.printA4(element.innerHTML);
             }
@@ -48,6 +65,7 @@ export class Print {
 
   organization: any;
   subcontractNote: any;
+  rowList = [];
 
   constructor(
     private service: SubcontractNoteService,
@@ -62,4 +80,27 @@ export class Print {
       this.organization = data[0];
     });
   }
+
+  makeRow(jobId, productTypeId, subcontractOperation){
+    return {
+      jobId : jobId,
+      productTypeId : productTypeId,
+      jobNo : subcontractOperation.job.jobNo,
+      quantity : subcontractOperation.quantity,
+      itemCode : subcontractOperation.subcontractOperationRate.subcontractorOperation.subcontractOperationDefinition.item.code,
+      productTypeDescription : subcontractOperation.subcontractOperationRate.subcontractorOperation.subcontractOperationDefinition.productType.description,
+      subRowList : [this.makeSubRow(subcontractOperation)]
+    };
+  }
+
+  makeSubRow(subcontractOperation){
+    return {
+      operationType : subcontractOperation.subcontractOperationRate.subcontractorOperation.subcontractOperationDefinition.operationType.description,
+      unitRate : subcontractOperation.subcontractOperationRate.rate,
+      quantity : subcontractOperation.quantity,
+      amount : subcontractOperation.quantity * subcontractOperation.subcontractOperationRate.rate
+    };
+  }
+
 }
+
