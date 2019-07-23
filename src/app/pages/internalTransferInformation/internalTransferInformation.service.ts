c29ec62de20fb4569fb43bcbdc771f76fcbef4a7
UmeshaAlawatta <umesha.nipunialawatta@gmail.com>
2019-07-23 18:31:41 +0530

    Internal Transfer Information Table Added

---
+++ src/app/pages/internalTransferInformation/internalTransferInformation.service.ts
@@ -0,0 +1,21 @@
+
+import { Injectable, Inject } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { HttpClient } from '@angular/common/http';
+import { APP_CONFIG, IAppConfig } from '../../app.config';
+import { AuthService } from '../../services/auth.service';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class InternalTransferInformationService extends MasterService {
+
+  constructor(private anHttp: HttpClient, @Inject(APP_CONFIG) private aConfig: IAppConfig, private anAuthService: AuthService) {
+    super(anHttp, aConfig, anAuthService);
+    this.setApiUrl('internalTransferItems/');
+  }
+
+  getInternalTransferInformation(fromLocation, toLocation, job, startDate, endDate, page, size): Observable<any> {
+    return this.http.get(this.apiUrl + 'internalTransferInformation?fromLocation=' + fromLocation + '&toLocation=' + toLocation +'&job=' +job + '&startDate=' + startDate + '&endDate=' + endDate + '&page=' + page + '&size=' + size, {headers: this.getJsonHeaders()})
+      .catch(err => this.handleError(err));
+  }
+}
