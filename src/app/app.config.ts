eec6ed752ecfae4ca1c469cb5389c7177db4c65b
Thilina <r.thilina@gmail.com>
2021-06-11 12:04:35 +0530

    make https ready

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,22 @@
-import { OpaqueToken } from '@angular/core';
+import {OpaqueToken} from '@angular/core';
 
 export let APP_CONFIG = new OpaqueToken('app.config');
 
 export interface IAppConfig {
   apiEndpoint: string;
 }
+
+let protocol = location.protocol;
 let hostname = location.hostname;
 let apiEndpoint =
   hostname === 'kpi.trwlanka.com' ||
   hostname === 'otr-kpi.firebaseapp.com' ||
   hostname === 'otr-kpi.nanosl.com' ||
   hostname === 'kpi.otrlanka.com'
-    ? 'http://api.otrlanka.com/'
+    ? protocol + '//api.otrlanka.com/'
     : hostname === '192.168.1.171'
-      ? 'http://' + hostname + '/api/'
-      : 'http://' + hostname + ':8080/';
+    ? protocol + '//' + hostname + '/api/'
+    : protocol + '//' + hostname + ':8080/';
 
 export const AppConfig: IAppConfig = {
   apiEndpoint: apiEndpoint
